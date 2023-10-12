cbd2254bcf0705dc9e2f8194734f9c611c94c465
Atlon1 <maciej.szajstek@gmail.com>
2023-10-12 21:32:04 +0200

    operation

--- src/components/Operations.tsx
+++ src/components/Operations.tsx
@@ -1,18 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {createOperation} from "../api/operations";
+import Operation from "./Operation";
 
 type OperationsProps = {
     taskID: number,
     form: boolean,
     setForm: React.Dispatch<React.SetStateAction<boolean>>,
     operations: any[],
-    setOperations:  React.Dispatch<React.SetStateAction<never[]>>
+    setOperations: React.Dispatch<React.SetStateAction<never[]>>
     status: string
 }
 const Operations = ({taskID, form, setForm, operations, setOperations, status}: OperationsProps) => {
+
+    const [operationDescription, setOperationDescription] = useState("");
+    const handleNewOperation = () => {
+
+        const operation = {
+            description: operationDescription,
+            timeSpent: 0,
+        }
+
+        createOperation(taskID, operation, (data: any) => {
+            // @ts-ignore
+            setOperations([...operations, data])
+            setOperationDescription("")
+            setForm(false)
+        })
+    }
+
+    const handleRemoveOperation = (id: number) => {
+        // @ts-ignore
+        setOperations(operations.filter(operation => operation.id !== id))
+    }
+
+    // @ts-ignore
     return (
-        <div>
-            operations
-        </div>
+        <>
+            {form && (
+                <div>
+                    <form>
+                        <div>
+                            <input
+                                type='text'
+                                placeholder='Operation description'
+                                value={operationDescription}
+                                onChange={(e) => setOperationDescription(e.target.value)}/>
+                            <div>
+                                <button onClick={handleNewOperation}>Add operation</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            )}
+            <ul>
+                {operations.map((operation) => (
+                    <div>
+                        <Operation key={operation.id} {...operation} onRemoveOperation={handleRemoveOperation} status={status}/>
+                    </div>
+                ))}
+            </ul>
+        </>
     );
 };
 
